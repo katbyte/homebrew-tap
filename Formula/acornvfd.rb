2745be4c32143b9a096057494bcfcbd23b964f73
acornvfd-release <acornvfd-release@katbyte.me>
2026-09-08 07:31:18 +0000

    acornvfd v0.1.2

--- Formula/acornvfd.rb
+++ Formula/acornvfd.rb
@@ -5,18 +5,18 @@
 class Acornvfd < Formula
   desc "Control the Acorn Workshop XGGF-1V48 VFD clock over Bluetooth LE"
   homepage "https://github.com/katbyte/acorn-vfd"
-  url "https://github.com/katbyte/acorn-vfd/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "54553710496e1b277525e2c146044d72e93975b8414e0b7e8ed91d739ba2b16f"
+  url "https://github.com/katbyte/acorn-vfd/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "a1a9ce71d213552a6768618eb8711fea7b713b6bf08fc40b77e16ab5bab8932f"
   license "GPL-3.0-only"
   head "https://github.com/katbyte/acorn-vfd.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/katbyte/acornvfd/lib/version.Version=v0.1.1 -X github.com/katbyte/acornvfd/lib/version.GitCommit=homebrew")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/katbyte/acornvfd/lib/version.Version=v0.1.2 -X github.com/katbyte/acornvfd/lib/version.GitCommit=homebrew")
   end
 
   test do
-    assert_match "v0.1.1", shell_output("#{bin}/acornvfd version")
+    assert_match "v0.1.2", shell_output("#{bin}/acornvfd version")
   end
 end
