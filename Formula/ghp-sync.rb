a2c406454ffe11b11bf12b27aae8f4b7ddc07965
ghp-sync-release <ghp-sync-release@katbyte.me>
2026-09-15 23:35:16 +0000

    ghp-sync v0.3.2

--- Formula/ghp-sync.rb
+++ Formula/ghp-sync.rb
@@ -5,18 +5,18 @@
 class GhpSync < Formula
   desc "Sync GitHub issues and PRs to a GitHub Project"
   homepage "https://github.com/katbyte/ghp-sync"
-  url "https://github.com/katbyte/ghp-sync/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "7c78382d2a15bce23a8ae7be3ad2eeda2b6675478925036a83bc932d6a6e1944"
+  url "https://github.com/katbyte/ghp-sync/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "34a1067f12c8b4cc582a923de63948855cfb841b3e9c7233cf37d1c7e57378e9"
   license "GPL-3.0-only"
   head "https://github.com/katbyte/ghp-sync.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/katbyte/go-kt/version.Version=v0.3.1 -X github.com/katbyte/go-kt/version.GitCommit=homebrew")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/katbyte/go-kt/version.Version=v0.3.2 -X github.com/katbyte/go-kt/version.GitCommit=homebrew")
   end
 
   test do
-    assert_match "v0.3.1", shell_output("#{bin}/ghp-sync version")
+    assert_match "v0.3.2", shell_output("#{bin}/ghp-sync version")
   end
 end
